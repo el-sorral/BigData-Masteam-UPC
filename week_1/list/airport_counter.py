863a02a93655ee40bece839bb189a3db42f35a60
Gerard Solé <g.sole.ca@gmail.com>
2017-02-16 18:32:26 +0100

    Adding list version

--- week_1/list/airport_counter.py
+++ week_1/list/airport_counter.py
@@ -1,28 +1,28 @@
 class AirportCounter:
-    airport_name = ''
+    name = ''
     totals = 0
     take_offs = 0
     landings = 0
 
-    def __init__(self, airport_name):
-        self.airport_name = airport_name
+    def __init__(self, name):
+        self.name = name
 
     def recompute_totals(self):
         self.totals = self.take_offs + self.landings
 
     def add_takeoff(self):
         self.take_offs += 1
-        self.recompute_totals()
+        self.totals += 1
 
     def add_landing(self):
         self.landings += 1
-        self.recompute_totals()
+        self.totals += 1
 
     def __eq__(self, item):
-        return self.airport_name == item
+        return self.name == item
 
     def __str__(self):
-        return self.airport_name + "\t" + str(self.totals) + "\t" + str(self.landings) + "\t" + str(self.take_offs)
+        return self.name + "\t" + str(self.totals) + "\t" + str(self.landings) + "\t" + str(self.take_offs)
 
     def __repr__(self):
-        return str(self)+"\n"
+        return str(self) + "\n"
--- week_1/list/main.py
+++ week_1/list/main.py
@@ -1,49 +1,55 @@
+#!/usr/bin/python
 import sys
-import csv
+import time
+import psutil
+import os
+import resource
 
 from airport_counter import AirportCounter
 
-file = sys.argv[1]
+t0 = time.time()
 
-print 'Opening file:', file
+filename = sys.argv[1]
 
-airport_list = []
+air_lst = list()
 
 
 def add_takeoff(airport_name):
-    if airport_name in airport_list:
-        i = airport_list.index(airport_name)
-        airport_list[i].add_takeoff()
-    else:
-        airport = AirportCounter(airport_name)
-        airport.add_takeoff()
-        airport_list.append(airport)
+    for airport in air_lst:
+        if airport.name == airport_name:
+            airport.add_takeoff()
+            return
+    airport = AirportCounter(airport_name)
+    airport.add_takeoff()
+    air_lst.append(airport)
     return
 
 
 def add_landing(airport_name):
-    if airport_name in airport_list:
-        i = airport_list.index(airport_name)
-        airport_list[i].add_landing()
-    else:
-        airport = AirportCounter(airport_name)
-        airport.add_landing()
-        airport_list.append(airport)
+    for airport in air_lst:
+        if airport.name == airport_name:
+            airport.add_landing()
+            return
+    airport = AirportCounter(airport_name)
+    airport.add_landing()
+    air_lst.append(airport)
     return
 
 
-def get_airport_key(airport_counter):
-    return airport_counter.totals
+with open(filename) as f:
+    for line in f:
+        line_split = line.split(';')
+        add_takeoff(line_split[0])
+        add_landing(line_split[1])
+    time_reading = time.time()
 
+print("AIRPORT NAME \t # TOTAL MOVEMENTS \t # TAKE OFFs \t # LANDINGS")
+for i in range(10):
+    idx = max(range(len(air_lst)), key=lambda j: air_lst[j].totals)
+    print air_lst[idx]
+    air_lst.remove(air_lst[idx])
 
-with open(file, 'rb') as csvfile:
-    reader = csv.reader(csvfile, delimiter=';')
-    for row in reader:
-        add_takeoff(str(row[0]))
-        add_landing(str(row[1]))
-
-    airport_list.sort(key=get_airport_key, reverse=True)
-
-    print "AIRPORT NAME \t # TOTAL MOVEMENTS \t # TAKE OFFs \t # LANDINGS"
-    for x in airport_list[:10]:
-        print "%s \t %d \t %d \t %d" % (x.airport_name, x.totals, x.take_offs, x.landings)
+print "Time processing", time.time() - time_reading
+print "Elapsed total time ", time.time() - t0
+info = psutil.Process(os.getpid()).memory_info().rss
+print "Memory footprint", info / 1000, "Kb"
