1c53f7f4add6dab0570043945fdf016778a91118
Gerard Solé <g.sole.ca@gmail.com>
2017-02-28 19:15:54 +0100

    Separate read from timmings

--- week_3/main.R
+++ week_3/main.R
@@ -1,14 +1,23 @@
 source("plain_R/top10airports.R")
+source("spark_R/top10airports.R")
 
 main <- function(filepath){
   print(filepath)
   flights_table <- as.data.frame(read.csv(filepath, header=FALSE, sep = ";"))
   plainR(flights_table)
+  sparkR
+}
+
+read <- function(filepath) {
+  return (as.data.frame(read.csv(filepath, header=FALSE, sep = ";")))
 }
 
 # Plain R
-plain_1_hour = system.time(main("./datasets/traffic1hour.exp2"))
-plain_1_day = system.time(main("./datasets/traffic1day.exp2"))
-plain_1_week = system.time(main("./datasets/traffic1week.exp2"))
+one_hour <- read("./datasets/traffic1hour.exp2")
+plain_1_hour = system.time(plainR(one_hour))
 
-print("D")
+one_day <- read("./datasets/traffic1day.exp2")
+plain_1_day = system.time(plainR(one_day))
+
+one_week <- read("./datasets/traffic1day.exp2")
+plain_1_week = system.time(plainR(one_week))
