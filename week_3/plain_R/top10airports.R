f8b8a99733628ae7da67f56561e254320c63e5e0
Gerard Solé <g.sole.ca@gmail.com>
2017-02-28 18:48:35 +0100

    Adding support from table to frame to add labels for merge

--- week_3/plain_R/top10airports.R
+++ week_3/plain_R/top10airports.R
@@ -4,20 +4,17 @@ plainR <- function(flights_table){
   #origin <- flights_table[,1]
   #destination <- flights_table[,2]
   
-  
   origin <- table(flights_table[,1])
-  
+  origin <- as.data.frame(origin)
   colnames(origin) <- c("Airport", "Origin")
-  print(origin)
-  
-  origin$V1 <- "Airport"
-  origin$V2 <- "Origin"
   
   destination <- table(flights_table[,2])
-  destination$V1 <- "Airport"
-  destination$V2 <- "Destination"
+  destination <- as.data.frame(destination)
+  colnames(destination) <- c("Airport", "Destiny")
   
   merged = merge(origin, destination, all=T)
+  
+  browser()
   
   print(merged)
 }
