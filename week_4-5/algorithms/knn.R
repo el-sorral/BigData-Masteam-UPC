201054b71764e1a6f99143aef198096f5d55931f
Gerard Solé <g.sole.ca@gmail.com>
2017-03-14 16:55:51 +0100

    Preserve changes

--- week_4-5/algorithms/knn.R
+++ week_4-5/algorithms/knn.R
@@ -1,5 +1,9 @@
 # load the library
 library(caret)
+library(doParallel)
+
+cl <- makeCluster(detectCores())
+registerDoParallel(cl)
 
 # load the dataset
 digits_csv <- read.csv("./digits.csv", head = FALSE)
@@ -9,7 +13,7 @@ train_data <- digits_csv[intrain, -65]
 train_labels <- as.factor(digits_csv[intrain, 65])
 
 # load test dataset
-test_csv <- read.csv("./digits.csv", head = FALSE)
+test_csv <- read.csv("./test.csv", head = FALSE)
 
 intest <- createDataPartition(y=test_csv[,65], p=1, list=FALSE)
 test_data <- digits_csv[intest, -65]
@@ -17,6 +21,17 @@ test_labels <- as.factor(digits_csv[intest, 65])
 
 # Train the model
 model <- train(train_data, train_labels, method = "knn")
+
+print.train(model)
+plot.train(model)
+
+set.seed(1)
+
+partGrid <- expand.grid(cp = (0:10)*0.01)
+knnGrid <- expand.grid(k = 1:5)
+
+grid <- expand.grid(k= 1:5, cp = (0:5)*0.1)
+
 
 # Predict values
 predictions <- predict(model, test_data)
@@ -30,4 +45,5 @@ head(output)
 res <- confusionMatrix(predictions, test_labels)
 res
 res$overall["Accuracy"]
+
 
